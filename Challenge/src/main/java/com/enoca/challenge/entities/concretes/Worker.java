2e6c7cd72b45fb96e16ca99a48bb559cd9209c7e
Hasan Soykan <hasannsoykan@gmail.com>
2023-04-12 01:47:58 +0300

    Update Worker.java

--- Challenge/src/main/java/com/enoca/challenge/entities/concretes/Worker.java
+++ Challenge/src/main/java/com/enoca/challenge/entities/concretes/Worker.java
@@ -30,7 +30,7 @@ public class Worker {
 	@Column(name = "last_name")
 	private String lastName;
 
-	@Column(name = "natioanal_identity", unique=true)
+	@Column(name = "national_identity", unique=true)
 	private String nationalIdentity;
 
 	@ManyToOne()
